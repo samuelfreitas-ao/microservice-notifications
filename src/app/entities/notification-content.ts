56cb9809dc4a10def0d4acd9d27e9bdc1513dc83
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-04 20:41:26 +0100

    feat: create notification model

---
+++ src/app/entities/notification-content.ts
@@ -0,0 +1,16 @@
+export class NotificationContent {
+  constructor(private readonly content: string) {
+    const isContentLengthValid = this.validadeContentLength(content);
+    if (!isContentLengthValid) {
+      throw new Error('Content length error.');
+    }
+  }
+
+  get value(): string {
+    return this.content;
+  }
+
+  private validadeContentLength(content: string): boolean {
+    return content.length > 5 && content.length < 248;
+  }
+}
---
+++ src/app/entities/notification.ts
@@ -0,0 +1,49 @@
+import { NotificationContent } from './notification-content';
+
+export interface NotificationProps {
+  recipientId: string;
+  content: NotificationContent;
+  category: string;
+  readAt: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  constructor(private props: NotificationProps) {}
+
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
+
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: NotificationContent) {
+    this.props.content = content;
+  }
+
+  public get content(): NotificationContent {
+    return this.props.content;
+  }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
+}
