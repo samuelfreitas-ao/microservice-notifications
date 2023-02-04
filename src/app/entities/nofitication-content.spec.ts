75a613ca993451fc9cfb3383d85fc1589963d82a
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-04 20:57:53 +0100

    test: should not be able to create a notification content with more than 240 characters

--- src/app/entities/nofitication-content.spec.ts
+++ src/app/entities/nofitication-content.spec.ts
@@ -11,4 +11,8 @@ describe('Notification Content', () => {
   it('should not be able to create a notification content with less than 5 characters', () => {
     expect(() => new NotificationContent('error')).toThrow();
   });
+
+  it('should not be able to create a notification content with more than 240 characters', () => {
+    expect(() => new NotificationContent('a'.repeat(241))).toThrow();
+  });
 });
