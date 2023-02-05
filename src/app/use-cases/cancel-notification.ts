e0c8a4c28cea67ba29257d26b8a095db2865fd9b
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-05 15:02:26 +0100

    feat: cancel notification usecase

---
+++ src/app/use-cases/cancel-notification.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationRepository } from '@app/repositories/notification-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+interface CancelNotificationRequest {
+  notificationId: string;
+}
+
+type CancelNotificationResponse = void;
+
+@Injectable()
+export class CancelNotification {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(
+    request: CancelNotificationRequest,
+  ): Promise<CancelNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.cancel();
+
+    await this.notificationRepository.save(notification);
+  }
+}
