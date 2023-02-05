45340961df16da73da85bf5886500b53103651fb
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-05 13:10:19 +0100

    refactor: use view model

--- src/infra/http/controller/notifications.controller.ts
+++ src/infra/http/controller/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { SendNotification } from '@app/use-cases/send-notification';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -15,6 +16,8 @@ export class NotificationsController {
       content,
       recipientId,
     });
-    return { notification };
+    return {
+      notification: NotificationViewModel.toHTTP(notification),
+    };
   }
 }
