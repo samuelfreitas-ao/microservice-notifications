940607b071e1ff5a4109347470d661a7f64ef55c
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-05 18:26:46 +0100

    feat: provide all notifications use cases to http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,4 +1,9 @@
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { CountRecipientNotification } from '@app/use-cases/count-recipient-notifications';
+import { GetRecipientNotification } from '@app/use-cases/get-recipiente-notification';
+import { ReadNotification } from '@app/use-cases/read-notification';
 import { SendNotification } from '@app/use-cases/send-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
 import { DatabaseModule } from '@infra/database/database.module';
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controller/notifications.controller';
@@ -6,6 +11,13 @@ import { NotificationsController } from './controller/notifications.controller';
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    ReadNotification,
+    UnreadNotification,
+    CountRecipientNotification,
+    GetRecipientNotification,
+    CancelNotification,
+  ],
 })
 export class HttpModule {}
