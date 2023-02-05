01db60c2abff427f1d2da6b886dd633ff40c11e5
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-05 14:58:49 +0100

    feat: find and save notification

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -15,4 +15,12 @@ export class PrismaNotificationRepository implements NotificationRepository {
       data,
     });
   }
+
+  async findById(notificationId: string): Promise<Notification> {
+    // throw new Error('Method not implemented.');
+  }
+
+  async save(notification: Notification): Promise<void> {
+    // throw new Error('Method not implemented.');
+  }
 }
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -7,4 +7,24 @@ export class InMemoryNotificationsRepository implements NotificationRepository {
   async create(notification: Notification): Promise<void> {
     this.notifications.push(notification);
   }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (item) => item.id === notificationId,
+    );
+    if (!notification) {
+      return null;
+    }
+    return notification;
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id == notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
 }
