efc1ef64b12a41b941dd33d1c6c323f0d376339e
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-05 13:02:50 +0100

    refactor: use notification mapper on db persistency

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -1,6 +1,7 @@
 import { Notification } from '@app/entities/notification';
 import { NotificationRepository } from '@app/repositories/notification-repository';
 import { Injectable } from '@nestjs/common';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mapper';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
@@ -8,15 +9,10 @@ export class PrismaNotificationRepository implements NotificationRepository {
   constructor(private prismaService: PrismaService) {}
 
   async create(notification: Notification): Promise<void> {
+    const data = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        category: notification.category,
-        content: notification.content.value,
-        recipientId: notification.recipientId,
-        readAt: notification.readAt,
-        createdAt: notification.createdAt,
-      },
+      data,
     });
   }
 }
