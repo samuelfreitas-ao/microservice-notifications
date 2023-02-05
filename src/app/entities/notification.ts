4bb26da6931ff34c7efd7d07fbd1c74deac73b56
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-05 17:10:32 +0100

    feat: read/unred notification

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -50,8 +50,12 @@ export class Notification {
     return this.props.category;
   }
 
-  public set readAt(readAt: Date | null | undefined) {
-    this.props.readAt = readAt;
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
   }
 
   public get readAt(): Date | null | undefined {
---
+++ src/app/use-cases/unread-notification.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationRepository } from '@app/repositories/notification-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+interface UnreadNotificationRequest {
+  notificationId: string;
+}
+
+type UnreadNotificationResponse = void;
+
+@Injectable()
+export class UnreadNotification {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(
+    request: UnreadNotificationRequest,
+  ): Promise<UnreadNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.unread();
+
+    await this.notificationRepository.save(notification);
+  }
+}
