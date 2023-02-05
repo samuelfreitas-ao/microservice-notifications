6c48a54c8b35502dad4fba22fe868a517cf30340
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-05 14:59:04 +0100

    feat: find and save notification

--- src/app/repositories/notification-repository.ts
+++ src/app/repositories/notification-repository.ts
@@ -2,4 +2,6 @@ import { Notification } from '@app/entities/notification';
 
 export abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
+  abstract save(notification: Notification): Promise<void>;
 }
