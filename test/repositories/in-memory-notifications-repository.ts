04595623df63dea194593e92c942fa8409ad4847
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-05 16:48:59 +0100

    feat: find notifications by recipientId

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -32,4 +32,10 @@ export class InMemoryNotificationsRepository implements NotificationRepository {
     return this.notifications.filter((item) => item.recipientId === recipientId)
       .length;
   }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (item) => item.recipientId === recipientId,
+    );
+  }
 }
